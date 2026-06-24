2f15f6cb543bfb2317480691a4606705ff9826df
Zine <mhmdmshwady14@gmail.com>
2026-06-24 14:36:22 +0300

    Update sixteen.sh

--- sixteen.sh
+++ sixteen.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-# تم حذف صمام الأمان تماماً عشان السكريبت يطنش الأخطاء ويكمل بناء زي الأول خالص
 
 if [ "$#" -lt 6 ]; then
     echo "Usage: $0 <STOCK_DEVICE> <USE_UI_8_TETHERING_APEX> <TARGET_DEVICE> <TARGET_DEVICE_CSC> <TARGET_DEVICE_IMEI> <OUTPUT_FILESYSTEM>"
     exit 1
 fi
 
-echo "📱 [1/4] جاري إعداد متغيرات الأجهزة والمسارات..."
 # Device info
-export STOCK_DEVICE=$(echo "$1" | xargs)
+export STOCK_DEVICE="$1"
 export USE_UI_8_TETHERING_APEX="$2"
-export TARGET_DEVICE=$(echo "$3" | xargs)
-export TARGET_DEVICE_CSC=$(echo "$4" | xargs)
-export TARGET_DEVICE_IMEI=$(echo "$5" | xargs)
+export TARGET_DEVICE="$3"
+export TARGET_DEVICE_CSC="$4"
+export TARGET_DEVICE_IMEI="$5"
 export OUTPUT_FILESYSTEM="$6"
 
 VERSION="1"
@@ -32,15 +30,13 @@ export BUILD_PARTITIONS="product,system_ext,system"
 source "$(pwd)/scripts/debloat.sh"
 source "$(pwd)/scripts/QuantumRom.sh"
 
-echo "📦 [2/4] جاري استخراج ملف Super.img وتفكيك الفيرموير..."
+#EXTRACT_FIRMWARE "$FIRM_DIR/$TARGET_DEVICE"
 EXTRACT_SUPER_IMG "$FIRM_DIR/$TARGET_DEVICE"
 EXTRACT_FIRMWARE_IMG "$FIRM_DIR/$TARGET_DEVICE" "all"
 
-echo "🧹 [3/4] جاري تنظيف الروم (Debloat) وتعديل ملفات الـ OMC..."
 DECODE_OMC "$FIRM_DIR/$TARGET_DEVICE"
 DEBLOAT "$FIRM_DIR/$TARGET_DEVICE"
 
-echo "🔧 [4/4] جاري تطبيق رقع الحماية وإضافة تطبيقات الفلاج شيب وتعديل الـ Jars..."
 APPLY_STOCK_CONFIG "$FIRM_DIR/$TARGET_DEVICE"
 PATCH_SELINUX "$FIRM_DIR/$TARGET_DEVICE"
 DISABLE_SECURITY "$FIRM_DIR/$TARGET_DEVICE"
@@ -65,13 +61,9 @@ mv -f "$WORK_DIR"/*.jar "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/"
 
 PATCH_BT_LIB "$FIRM_DIR/$TARGET_DEVICE" "$WORK_DIR"
 
-# تعديل الـ build.prop
 B_ID="$(grep -m1 '^ro.system.build.id=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
 B_V="$(grep -m1 '^ro.system.build.version.incremental=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
 BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.build.display.id" "${B_ID} ${B_V} V-${VERSION}: ZineROM"
 BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.build.display.id" "${B_ID} ${B_V} V-${VERSION}: ZineROM"
 
-echo "💾 [5/5] جاري بناء وتجميع ملفات الـ .img النهائية (BUILD_IMG)..."
 BUILD_IMG "$FIRM_DIR/$TARGET_DEVICE" "all" "$OUTPUT_FILESYSTEM" "$OUT_DIR"
-
-echo "✅ تم الانتهاء من سكريبت البناء بالكامل!"
